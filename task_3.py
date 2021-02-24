5afa243dbbc51497e433e37d304432b950714759
antonshe84 <antonshe84@gmail.com>
2021-02-24 22:54:43 +0300

    intermediate

--- task_3.py
+++ task_3.py
@@ -11,18 +11,13 @@
  в ответе сервера. Дата должна быть в виде объекта date. Подумайте, как извлечь дату из ответа, какой тип данных лучше
   использовать в ответе функции?
 """
-from decimal import Decimal
-
-"""
-<?xml version="1.0" encoding="windows-1251"?><ValCurs Date="21.02.2021" name="Foreign Currency Market">
-<Valute ID="R01010"><NumCode>036</NumCode><CharCode>AUD</CharCode><Nominal>1</Nominal><Name>Австралийский доллар</Name><Value>58,2249</Value></Valute>
-<Valute ID="R01020A"><NumCode>944</NumCode><CharCode>AZN</CharCode><Nominal>1</Nominal><Name>Азербайджанский манат</Name><Value>43,5196</Value></Valute>
-"""
 
 import requests
-from datetime import datetime, date
+from datetime import date
+import sys
 
 serv = 'http://www.cbr.ru/scripts/XML_daily.asp'
+
 
 def find_tag(st, tag1, tag2):
     t1 = st.find(tag1)
@@ -35,37 +30,44 @@ def find_tag(st, tag1, tag2):
     else:
         return None
 
+
 def currency_rates(val):
-	# Получаем текст запроса
-	response = requests.get(serv)
-	s = response.text
-	date_val = find_tag(s, '<ValCurs Date="', '"')   	# 21.02.2021
-	date_return = date(int(date_val[6:10]), int(date_val[3:5]), int(date_val[0:2]))
-	s = s[s.find('<Valute'):]
-	valute = find_tag(s, '<Valute', '</Valute>')
-	valute_course = {}
-	while valute:
-		valute_course[find_tag(valute, '<CharCode>', '</CharCode>').lower()] = \
-			float(find_tag(valute, '<Value>', '</Value>').replace(",", "."))
-		s = s[len(valute) + 16:]
-		valute = find_tag(s, '<Valute', '</Valute>')
-	course_ret = valute_course.get(val.lower())
-	if course_ret is None:
-		return None
-	else:
-		return [course_ret, date_return]
+    """
+	Функция возвращает курс валюты и дату
+
+	:param val: валюта
+	:return: [курс в рублях, дата]
+	"""
+    # Получаем текст запроса
+    response = requests.get(serv)
+    s = response.text
+    # ищем дату по тегу
+    d = find_tag(s, '<ValCurs Date="', '"')  # 21.02.2021
+    date_return = date(int(d[6:10]), int(d[3:5]), int(d[0:2]))
+    # Далее поиск и добавление в словарь всех валют и курсов в рублях
+    s = s[s.find('<Valute'):]
+    valute = find_tag(s, '<Valute', '</Valute>')
+    valute_course = {}
+    while valute:
+        valute_course[find_tag(valute, '<CharCode>', '</CharCode>').lower()] = \
+            float(find_tag(valute, '<Value>', '</Value>').replace(",", "."))
+        s = s[len(valute) + 16:]
+        valute = find_tag(s, '<Valute', '</Valute>')
+    # Получение курса заданной валюты
+    course_ret = valute_course.get(val.lower())
+    if course_ret is None:
+        return None
+    else:
+        return [course_ret, date_return]
 
 
 if __name__ == '__main__':
-	while True:
-		v = input("Введите валюту (например USD), для выхода наберите 'exit': ")
-		if v == "exit": break
-		d = currency_rates(v)
-		if d is None:
-			print("Валюта указана не верно, повторите ввод")
-		else:
-			print(f'{d[0]:0.2f}, {d[1].strftime("%Y-%m-%d")}')
 
-
-
-
+    while True:
+        v = input("Введите валюту (например USD), для выхода наберите 'exit': ")
+        if v == "exit": break
+        d = currency_rates(v)
+        if d is None:
+            print("Валюта указана не верно, повторите ввод")
+        else:
+            print(f'{d[0]:0.2f}, {d[1].strftime("%Y-%m-%d")}')
--- task_5.py
+++ task_5.py
@@ -7,3 +7,23 @@
 75.18, 2020-09-05
 """
 
+from datetime import date
+import sys
+from course import *
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    while True:
+        if len(args) > 1:
+            v = args[1]
+        else:
+            v = input("Введите валюту (например USD), для выхода наберите 'exit': ")
+        if v == "exit": break
+        d = currency_rates(v)
+        if d is None:
+            print("Валюта указана не верно, повторите ввод")
+        else:
+            print(f'{d[0]:0.2f}, {d[1].strftime("%Y-%m-%d")}')
+        if len(args) > 1: break
